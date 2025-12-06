9dc605f6f5afb4ba56d7fd68b52e80d55b2f9f3e
Aidan <aidanlciesla@gmail.com>
2025-12-05 19:04:14 -0800

    Data is read from file

--- part 2/main.cpp
+++ part 2/main.cpp
@@ -7,21 +7,38 @@
 
 using namespace std;
 
-vector<double> convertIEEE(const vector<string>& inNums) {
-    vector<double> double_list;
-    // Use stod to convert the string (even in scientific notation) to a double
-    for (const string& str : inNums) double_list.push_back(stod(str));
-    return double_list;
+struct dataPoint {
+    vector<double> data;
+    double pClass;
+};
+
+struct classifier {
+    void train();
+    void test(const int&);
+};
+
+void classifier::train() {
+
+}
+
+void classifier::test(const int& k) {
+
 }
 
 int main() {
     cout << "Which dataset would you like to use?\n1. Small\n2. Large\n";
-    unsigned opinion;
+    unsigned opinion, n;
     cin >> opinion;
 
     string filename;
-    if (opinion == 1) filename = "small-test-dataset.txt";
-    else filename = "large-test-dataset.txt";
+    if (opinion == 1) {
+        filename = "small-test-dataset.txt";
+        n = 10;
+    }
+    else {
+        filename = "large-test-dataset.txt";
+        n = 40;
+    }
 
     cout << "List the features you would like to use (insert 0 to finish): ";
     vector<int> features;
@@ -35,19 +52,20 @@ int main() {
 
     while (features.back() > 10 && opinion == 1) features.pop_back(); //only features 1-10 for small dataset
 
-
-    ifstream inFile(filename);
-    ofstream outFile("large-test-dataset-2.txt");
-    while (!inFile.eof() && inFile.is_open()) {
-        vector<string> inLines;
-        string inLine;
-        inLines.resize(41);
-        for (unsigned i = 0; i < 41; i++) {
-            inFile >> inLine;
-            inLines[i] = inLine;
+    //Populate dataset
+    vector<dataPoint> data;
+    ifstream inData(filename);
+    if (!inData.is_open()) throw runtime_error("oops");
+    while (inData.is_open() && !inData.eof()) {
+        dataPoint newDP;
+        inData >> newDP.pClass;
+        for (unsigned i = 0; i < n; i++) {
+            double featPoint;
+            inData >> featPoint;
+            newDP.data.push_back(featPoint);
         }
-        vector<double> output = convertIEEE(inLines);
-        for (auto d : output) outFile << d << "  ";
-        outFile << endl;
-    }
+        data.push_back(newDP);
+    } data.pop_back();
+
+    
 }
